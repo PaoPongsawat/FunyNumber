78818ceed9e00b5177e7cb22b67c8d453abd42fb
Com422Mac10 <com422mac10@Com422Mac10s-iMac.local>
2017-07-27 16:52:53 +0700

    Commit Method

---
+++ FunyNumber/MyClass.swift
@@ -0,0 +1,23 @@
+//
+//  MyClass.swift
+//  FunyNumber
+//
+//  Created by Com422Mac10 on 7/27/2560 BE.
+//  Copyright © 2560 Com422Mac10. All rights reserved.
+//
+
+import Foundation
+class MyClass {
+    let strName = "Doremon"
+    
+    
+    //DecreAW Number
+    func decreaseNumber(intNumber:Int) -> Int {
+        
+        let intResult = intNumber - 1
+        return intResult
+        
+    }
+    
+    
+}//My Class
--- FunyNumber/ViewController.swift
+++ FunyNumber/ViewController.swift
@@ -9,10 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
+    @IBAction func decButton(_ sender: Any) {
+        let strNumber = numberLabel.text
+        let intNumber = Int(strNumber!)
+        
+        let myClass = MyClass()
+        let intNewNumber = myClass.decreaseNumber(intNumber: intNumber!)
+        print("Number ==> \(intNewNumber)")
+        
+        
+        
+        
+        
+        
+        
+        
+    }//decButton
+    
+    
+    @IBAction func incButton(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let testClass = MyClass()
+        print("Name ==> \(testClass.strName)")
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
